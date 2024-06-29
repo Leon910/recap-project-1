bef038a7e8e1e41cc394db0e52c424a52306baea
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-06-29 17:04:17 +0200

    add function counter buttons

--- script.js
+++ script.js
@@ -33,6 +33,67 @@ const answer = document.querySelector('[data-js="show-answer"]');
 // forEach button
 // -> here comes my code
 
+/* Count Questions*/
+
+// If we are on the questions page, count the bookmark boxes and store in localStorage
+
+document.addEventListener("DOMContentLoaded", function () {
+  const questionBoxList = document.querySelector(
+    '[data-js="count-question-box"]'
+  );
+  if (questionBoxList) {
+    const questionBoxes = questionBoxList.children.length;
+    localStorage.setItem("questionBoxCount", questionBoxes);
+
+    console.log("Question Box Count:", questionBoxes);
+  }
+});
+
+// If we are on the profile page, retrieve and display the bookmark count on the button
+const countDisplayButton = document.querySelector(
+  '[data-js="count-created-questions"]'
+);
+if (countDisplayButton) {
+  const questionBoxCountRetrieve =
+    localStorage.getItem("questionBoxCount") || 0;
+  countDisplayButton.textContent = `Created Questions (${questionBoxCountRetrieve})`;
+
+  console.log(
+    "Question Box Count from Local Storage:",
+    questionBoxCountRetrieve
+  );
+}
+
+/* Count Bookmarks*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  // If we are on the bookmarks page, count the bookmark boxes and store in localStorage
+  const bookmarkBoxList = document.querySelector(
+    '[data-js="count-bookmark-box"]'
+  );
+  if (bookmarkBoxList) {
+    const bookmarkBoxes = bookmarkBoxList.children.length;
+    localStorage.setItem("bookmarkBoxCount", bookmarkBoxes);
+
+    console.log("Bookmark Box Count:", bookmarkBoxes);
+  }
+
+  // If we are on the profile page, retrieve and display the bookmark count on the button
+  const countDisplayButton = document.querySelector(
+    '[data-js="count-created-bookmarks"]'
+  );
+  if (countDisplayButton) {
+    const bookmarkBoxCountRetrieve =
+      localStorage.getItem("bookmarkBoxCount") || 0;
+    countDisplayButton.textContent = `Created Bookmarks (${bookmarkBoxCountRetrieve})`;
+
+    console.log(
+      "Bookmark Box Count from Local Storage:",
+      bookmarkBoxCountRetrieve
+    );
+  }
+});
+
 toggleAnswerButton.addEventListener("click", () => {
   if (answer.style.visibility === "hidden") {
     answer.style.visibility = "visible";
@@ -40,5 +101,3 @@ toggleAnswerButton.addEventListener("click", () => {
     answer.style.visibility = "hidden";
   }
 });
-
-console.log(answerButtonSelect);
